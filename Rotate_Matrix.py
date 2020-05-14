42c17bd0d4287dc35ab5bf3d3289de33014c3e7a
Saba Fathi <saba.fathi@emailgmail.info>
2020-05-13 21:07:19 -0400

    bruh moment

--- Rotate_Matrix.py
+++ Rotate_Matrix.py
@@ -1,13 +1,34 @@
 
 
-# this program rotates an image of len by width by 90 degrees
+'''
+    given a NxN matrix, rotate the numbers by 90 degrees
+    difficult
+'''
+# input matrix
+matrix = [[1, 2, 3],
+          [4, 5, 6],
+          [7, 8, 9]]
 
-arrow = ' ---> '
-len = 3
-width = 5
+# assigning variables 
+len_matrix = len(matrix)
+cols = 0
+ret_matrix = []
 
-for i in range(len):
-    
-    for j in range(width):
+# creating the blank matrix
+for i in range(len_matrix):
+    ret_matrix.append([])
+
+# appending numbers to blank matrix
+for i in range(len_matrix):
+    rows = len(matrix) - 1
+
+    # iterates through the given column of each nested list
+    while rows >= 0:
         
-        print(str(i) + ' ' + str(j) + arrow) 
+        # appending number
+        ret_matrix[i].append(matrix[rows][cols])
+        rows -= 1
+    # change the column
+    cols += 1
+
+print(ret_matrix)
