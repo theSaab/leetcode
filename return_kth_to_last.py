52f571129f58b16c37a2636b6cfec3e3be173864
Saba Fathi <saba.fathi@emailgmail.info>
2020-07-16 13:03:24 -0400

    completed suM of lists, not fun

--- return_kth_to_last.py
+++ return_kth_to_last.py
@@ -3,10 +3,10 @@
 
 from collections import deque
 
-input_list = deque('123456')
+input_list = deque('125555555553456')
 
 def kth_to_last(list, kth):
 
-    return(list[-(kth+1)])
+    print(list[-(kth+1)])
 
-kth_to_last(input_list, 2)
+kth_to_last(input_list, 14)
--- sum_lists.py
+++ sum_lists.py
@@ -1,7 +1,13 @@
 
+# This program takes 2 linked lists, list 1 and list2, and where each node contains a single
+# digit. The digits are stored in reverse order, such that the 1 's digit is at the head of the list. Write a
+# function that adds the two numbers and returns the sum as a linked list.
+
+
 from collections import deque
 
-class LinkedLists:
+
+class LinkedList:   # Basic LinkedLists creation class
     def __init__(self, nodes=None):
         self.head = None
         if nodes is not None:
@@ -11,6 +17,15 @@ class LinkedLists:
                 node.next = Node(data=elem)
                 node = node.next
 
+    def __repr__(self):
+        node = self.head
+        nodes = []
+        while node is not None:
+            nodes.append(node.data)
+            node = node.next
+        nodes.append("None")
+        return " -> ".join(nodes)
+
     def __iter__(self):
         node = self.head
         while node is not None:
@@ -18,32 +33,46 @@ class LinkedLists:
             node = node.next
 
 
-class Node:
+class Node:   # also basic Node creation
     def __init__(self, data):
         self.data = data
         self.next = None
 
-Llist = LinkedLists()
-
-num1_1 = Node(deque(123))
-num1_2 = Node(2)
-num1_3 = Node(3)
-num2_1 = Node(4)
-num2_2 = Node(5)
-num2_3 = Node(6)
-
-num1_1.next = num1_2
-num1_2.next = num1_3
-
-num2_1.next = num2_2
-num2_2.next = num2_3
-
-first_node = num1
-Llist.head = first_node
+    def __repr__(self):
+        return self.data
 
 
+def create_num(self):
+    # This function turns the linked list into a number to be used later
+    num = ''
+    for node in self:
+        if node != None:  
+            num = str(node) + num
+    return int(num)
 
 
+def add_nums(first, second):
+    # this function takes 2 numbers and returns the addition as a string
+    list_sum = []
+    num3 = str(first + second)
 
-for elem in Llist:
-    print(elem.data)
+    for char in num3[len(num3)::-1]:
+        list_sum.append(char)
+
+    print(list_sum)
+
+    # presents the linked list, final product
+    print(LinkedList(list_sum))
+
+
+def sum_lists(list1, list2):
+    list1 = LinkedList(list1)
+    list2 = LinkedList(list2)
+
+    num1 = create_num(list1)
+    num2 = create_num(list2)
+
+    add_nums(num1, num2)
+
+
+sum_lists(['7', '1', '6'], ['5', '9', '2'])
