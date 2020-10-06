41b977846a4d42fb47961f35ee1e3f923bc35f44
theSaab <saba.fathi@emailgmail.info>
2020-10-06 19:55:23 -0400

    SWE_INTERN

--- SWE_intern.py
+++ SWE_intern.py
@@ -1,18 +1,15 @@
 
-
-def remove(arr, k):
-
-    index_count = 0
-    count = 0
-    while len(arr) > 1:
+# Given an array of 0's and 1's, move all of the 0's to the beginning of the array and all of the 1's to the end of the array.
+def move0( arr ):
+    j = 0
+    i = 0
+    while j < len(arr):
         
-        count += 1
-        if count == k:
-            arr.pop(index_count%len(arr))
-            count = 1
-        index_count += 1
-        print(arr)
+        if arr[i] == 1:
+            # print('here')
+            del arr[i]
+            i -= 1
+            arr.append(1)
+        j += 1
+        i += 1
     return arr
-
-
-print(remove([1,2,3,4,5,6,7], 4))
